03b6bba15679ef3daa42b0b568311d79480017a5
Vitor Gonçalves <vitorfgoncalves3@gmail.com>
2020-06-22 20:49:55 -0300

    correção do bug onde snake desaparecia do canvas, organização do código

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 let canvas = document.getElementById("snake");
+
 let context = canvas.getContext("2d");
+
 let box = 32;
+
 let snake = [];
 snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
 
-let direction = "right";
+let direction;
+
 let food = {
     x: Math.floor(Math.random() * 15 + 1) * box,
     y: Math.floor(Math.random() * 15 + 1) * box
 }
+// Funções de desenho
 
 function criarBG() {
     context.fillStyle = "lightgreen";
-    context.fillRect(0, 0, 16 * box, 16 * box);
+    context.fillRect(0, 0, 17 * box, 17 * box);
 }
 
 function criarCobrinha() {
@@ -30,6 +35,7 @@ function drawFood() {
     context.fillRect (food.x, food.y, box, box);
 }
 
+// Fim das funções de desenho
 document.addEventListener('keydown', update);
 
 function update(event) {
@@ -40,10 +46,14 @@ function update(event) {
 }
 
 function iniciarJogo() {
+    criarBG();
+    criarCobrinha();
+    drawFood();
+    
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
-    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].x <= 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
-    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+    if(snake[0].y <= 0 && direction == "up") snake[0].y = 16 * box;
 
     for (i = 1; i < snake.length; i++){
         if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
@@ -51,10 +61,6 @@ function iniciarJogo() {
             alert('Game over!');
         }
     }
-
-    criarBG();
-    criarCobrinha();
-    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
@@ -82,5 +88,5 @@ function iniciarJogo() {
     snake.unshift(newHead);
 }
 
-let jogo = setInterval(iniciarJogo, 100);
+let jogo = setInterval(iniciarJogo, 50);
 
